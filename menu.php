f5164cabc69471dbaa3242aae5d06434858d3809
Vahvel <tallatatt@gmail.com>
2015-03-08 11:07:11 +0200

    html5 validaatori parandused vol2

--- menu.php
+++ menu.php
@@ -1,15 +1,13 @@
-<!DOCTYPE html>
-	<html>
-		<nav>				
-				<img src="img/arrow.png" alt="arrow">		
-				<br>
-				<a href="test.php"> Logi sisse </a><br>
-				<a href="index.php"> Kandidaadi lisamine </a><br>
-				<a href="#"> Hääleta </a><br>
-				<a href="#"> Tühista hääl </a><br>
-				<a href="#"> Kandidaadid </a><br>
-				<a href="#"> Otsing </a><br>
-				<a href="#"> Tulemuste jälgimine </a><br>
-				<a href="#"> Statistika </a>				
-			</nav>
-	</html>
+<nav>				
+	<img src="img/arrow.png" alt="arrow">		
+	<br>
+	<a href="test.php"> Logi sisse </a><br>
+	<a href="index.php"> Kandidaadi lisamine </a><br>
+	<a href="#"> Hääleta </a><br>
+	<a href="#"> Tühista hääl </a><br>
+	<a href="#"> Kandidaadid </a><br>
+	<a href="#"> Otsing </a><br>
+	<a href="#"> Tulemuste jälgimine </a><br>
+	<a href="#"> Statistika </a>				
+</nav>
+
