ce23f3702b897545bde4fa0ae7b4a451c5ab10d0
Olari <olari93@gmail.com>
2015-03-24 22:56:20 +0200

    Kandidaadi lisamine

--- public/menu.php
+++ public/menu.php
@@ -2,10 +2,10 @@
 	<ul class="text-center">
         	<li><a href="index.php">Avaleht</a></li>
           	<li><a href="login.php">Logi sisse</a></li>
-          	<li><a href="index.php">Kandidaadi lisamine</a></li>
+          	<li><a href="kandidaadid.php">Kandidaadid</a></li>
+            <li><a href="addkandidaadid.php">Kandidaadi lisamine</a></li>
           	<li><a href="#">Hääleta</a></li>
           	<li><a href="#">Tühista hääl</a></li>
-          	<li><a href="kandidaadid.php">Kandidaadid</a></li>
           	<li><a href="#">Otsing</a></li>
           	<li><a href="#">Tulemuste jälgimine</a></li>
           	<li><a href="#">Statistika</a></li>
