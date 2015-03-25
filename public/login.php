f9041fb4cde2901762f22d712a66f8f3fc289668
Olari <olari93@gmail.com>
2015-03-25 11:01:24 +0200

    Eraldasin login php

--- public/login.php
+++ public/login.php
@@ -14,17 +14,7 @@
     </div>			
 	<div class="body">
 		<div class="container">	
-			<?php
-				require __DIR__.'/../bootstrap/autoload.php';
-				$app = require_once __DIR__.'/../bootstrap/app.php';
-				$kernel = $app->make('Illuminate\Contracts\Http\Kernel');
-
-				$response = $kernel->handle(
-					$request = Illuminate\Http\Request::capture()
-				);
-				$response->send();
-				$kernel->terminate($request, $response);				
-			?>		
+			<?php include('loginphp.php');?>		
 		</div>
 	</div>
 
