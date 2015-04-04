a27c065f198c8df1186c973f3a29c5040661dea2
Vahvel <tallatatt@gmail.com>
2015-04-05 02:47:24 +0300

    siiiigh

--- resources/views/notloggedin.blade.php
+++ resources/views/notloggedin.blade.php
@@ -3,8 +3,8 @@
 
 <div class="container">   
     <p>
-		{!!HTML::link('/login','Logi sisse',['class'=>'btn btn-link'])!!} või {!!HTML::link('/register','Registreeri kasutajaks',['class'=>'btn btn-link'])!!}
-		<p>{!!HTML::link('/facebookauth','Logi sisse facebook kontoga',['class'=>'btn btn-link'])!!}</p> 
+		{!!HTML::link('/login','Logi sisse',['class'=>'btn btn-default'])!!} või {!!HTML::link('/register','Registreeri kasutajaks',['class'=>'btn btn-default'])!!}
+		<p>{!!HTML::link('/facebookauth','Logi sisse facebook kontoga',['class'=>'btn btn-default'])!!}</p> 
     </p>    
 </div>
 
