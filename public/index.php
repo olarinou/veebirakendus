f10359ef50a1761db7229b635fc05d93fe7b77a6
Vahvel <tallatatt@gmail.com>
2015-04-05 23:57:51 +0300

    html validator fix attempt v3

--- public/index.php
+++ public/index.php
@@ -1,5 +1,11 @@
 <!DOCTYPE html>
-<html>
+<head>	
+    <meta charset="utf-8">
+	<title>Veebivalimised</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">   
+	<link rel="stylesheet" href="{{ URL::asset('css/bootstrap.min.css') }}">	
+	<link rel="stylesheet" type="text/css" href="{{ URL::asset('theme.css') }}">
+</head>
 	<body>
 	<div class="body">
 		<div class="container">	
