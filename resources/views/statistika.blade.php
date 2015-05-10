57b23e09f4edd6a4144071b1aa8ddd7f761bba97
Vahvel <tallatatt@gmail.com>
2015-05-10 19:49:05 +0300

    test

--- resources/views/statistika.blade.php
+++ resources/views/statistika.blade.php
@@ -1,8 +1,10 @@
 @extends('master') 
 @section('main')
 	<div class="container">
+	@section('scripts')
 	@include('statistikastream') 
     <!--<div id="perf_div"></div>
    {!! $lava->render('PieChart', 'Info', 'perf_div') !!}-->
 	</div>
+	@stop
 @stop
--- resources/views/statistikastream.blade.php
+++ resources/views/statistikastream.blade.php
@@ -5,7 +5,7 @@ var source = new EventSource("/data");
 source.onmessage = function(event) {
 document.getElementById("result").innerHTML = event.data;
 };
-	} else {
-    document.getElementById("result").innerHTML = "Sorry, your browser does not support server-sent events...";
-	}
-	</script>
+} else {
+document.getElementById("result").innerHTML = "Sorry, your browser does not support server-sent events...";
+}
+</script>
