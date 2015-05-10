1d17bcedace4d6813c4a9dcaae699fd765d740ec
Vahvel <tallatatt@gmail.com>
2015-05-10 20:28:00 +0300

    test

--- resources/views/statistika.blade.php
+++ resources/views/statistika.blade.php
@@ -2,8 +2,15 @@
 @section('main')
 	<div class="container">
 	<p id="result"></p>
-	@section('scripts')	
-	{{ HTML::script('js/statsstream.js') }}
-	@stop
+	<script>
+if(typeof(EventSource) !== "undefined") {
+var source = new EventSource("/data");
+source.onmessage = function(event) {
+document.getElementById("result").innerText = event.data;
+};
+} else {
+document.getElementById("result").innerText = "Sorry, your browser does not support server-sent events...";
+}
+</script>
 	</div>
 @stop
