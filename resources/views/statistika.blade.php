0037ae54657d8454bec6624965ec462dce3382f9
Vahvel <tallatatt@gmail.com>
2015-05-10 21:44:38 +0300

    test

--- resources/views/statistika.blade.php
+++ resources/views/statistika.blade.php
@@ -2,13 +2,24 @@
 <script src="js/statsstream.js"></script>
 @section('main')
 	<div class="container">
-		<select name="valik" onchange="dataRetrival(valik)">
+		<select name="valik">
 			<option value="Riik">Riigis</option>
     		<option value="Piirkond">Piirkondades</option>
     		<option value="Partei">Parteides</option>
    			<option value="Kandidaat">Kandidaatides</option>   						
 		</select>
 	<br>
-	<p id="result" onLoad="dataRetrival(valik)"></p>
+	<p id="result"></p>
+	<script>
+	if(typeof(EventSource) !== "undefined") {
+		var source = new EventSource("/data/"+querySetting);
+		source.onmessage = function(event) {
+			document.getElementById("result").innerText = event.data;
+		};
+	}
+	else {
+		document.getElementById("result").innerText = "Sorry, your browser does not support server-sent events...";
+	}
+	</script>
 	</div>
 @stop
