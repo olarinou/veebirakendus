1cc19371f29cf48f1541de50716d9152225d952f
Vahvel <tallatatt@gmail.com>
2015-05-10 19:35:25 +0300

    test

---
+++ resources/views/statistikastream.blade.php
@@ -0,0 +1,11 @@
+<div id="result"></div>
+<script>
+if(typeof(EventSource) !== "undefined") {
+var source = new EventSource("/data");
+source.onmessage = function(event) {
+document.getElementById("result").innerHTML = event.data;
+};
+	} else {
+    document.getElementById("result").innerHTML = "Sorry, your browser does not support server-sent events...";
+	}
+	</script>
