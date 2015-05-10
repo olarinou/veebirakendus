632956e3cd10bc75a278d5ef6d5cb977eada9bb4
Vahvel <tallatatt@gmail.com>
2015-05-11 01:49:07 +0300

    ._.

--- public/js/statsstream.js
+++ public/js/statsstream.js
@@ -1,15 +1,21 @@
 
 
 function dataRetrival() {	
-	if(typeof(EventSource) !== "undefined") {
+	if(typeof(EventSource) !== "undefined") {	
 		var e = document.getElementById("valik");
 		var querySetting = e.options[e.selectedIndex].value;
 		var source = new EventSource("/data/"+querySetting);		
 		source.onmessage = function(event) {
-			document.getElementById("result").innerText = event.data;					
+			document.getElementById("result").innerText = event.data;
+			source.close();
 		};
+		 
 	}
 	else {
 		document.getElementById("result").innerText = "Sorry, your browser does not support server-sent events...";
 	}
+}
+
+function incrediblystupidloop(){
+	dataRetrival();
 }
