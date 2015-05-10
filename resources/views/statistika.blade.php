6602421e1bf62b0138fd386cd677b06ce4d4f35c
Vahvel <tallatatt@gmail.com>
2015-05-11 01:00:53 +0300

    test

--- resources/views/statistika.blade.php
+++ resources/views/statistika.blade.php
@@ -2,16 +2,13 @@
 <script src="js/statsstream.js"></script>
 @section('main')
 	<div class="container">
-		<select id="valik">
+		<select id="valik" onchange="dataRetrival(this.value)">
 			<option value="Riik">Riigis</option>
     		<option value="Piirkond">Piirkondades</option>
     		<option value="Partei">Parteides</option>
    			<option value="Kandidaat">Kandidaatides</option>   						
 		</select>
 	<br>
-	<p id="result"></p>		
-	<script type="text/javascript">
-		dataRetrival();
-	</script>	
+	<p id="result"></p>
 	</div>
 @stop
