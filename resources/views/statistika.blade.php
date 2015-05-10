f07b91c4ef7a6784cd3cca178957d61b6ccb9f8d
Vahvel <tallatatt@gmail.com>
2015-05-10 23:11:43 +0300

    test

--- resources/views/statistika.blade.php
+++ resources/views/statistika.blade.php
@@ -2,7 +2,7 @@
 <script src="js/statsstream.js"></script>
 @section('main')
 	<div class="container">
-		<select name="valik" onload="dataRetrival("Riik")" onchange="dataRetrival.querySetting = value">
+		<select name="valik" onchange="dataRetrival.querySetting = value">
 			<option value="Riik">Riigis</option>
     		<option value="Piirkond">Piirkondades</option>
     		<option value="Partei">Parteides</option>
@@ -10,5 +10,6 @@
 		</select>
 	<br>
 	<p id="result"></p>
+	dataRetrival();
 	</div>
 @stop
