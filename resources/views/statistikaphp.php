deeac04219f862618ddf100602683cc17b6b599d
supisaurus <supisau@ut.ee>
2015-05-10 15:27:54 +0300

    Update statistikaphp.php
    -__________-

--- resources/views/statistikaphp.php
+++ resources/views/statistikaphp.php
@@ -2,33 +2,34 @@
 $mysqli = NEW MySQLi('localhost','root','Admin123','vv_db');
 
 $infoTable = Lava::DataTable();
-	$infoTable->addStringColumn('Piirkond')
-			->addStringColumn('Kandidaat') 
-            ->addStringColumn('Erakond') 
-            ->addNumberColumn('Hääled');	
+$infoTable->addStringColumn('Piirkond')
+	->addStringColumn('Kandidaat') 
+        ->addStringColumn('Erakond') 
+        ->addNumberColumn('Hääled');	
 	
 	//		 	
-	$query = $mysqli->query("SELECT k.kandidaadiID, k.nimi, k.erakond, k.piirkond, t.tulemus FROM kandidaadid as k
-	JOIN tulemused as t ON (k.kandidaadiID = t.kandidaadiID)");
-	if($query->num_rows != 0){		
+$query = $mysqli->query("SELECT k.kandidaadiID, k.nimi, k.erakond, k.piirkond, t.tulemus FROM kandidaadid as k
+JOIN tulemused as t ON (k.kandidaadiID = t.kandidaadiID)");
+if($query->num_rows != 0){		
+	while($rows = $query->fetch_assoc()){ 
+		$kandid= $rows['kandidaadiID'] ;
+		$nimi=$rows['nimi'];
+		$erakond=$rows['erakond'];
+		$piirkond=$rows['piirkond']; 
+		$tulemus=$rows['tulemus']; 
+		$rowData = array(
+		$piirkond, $nimi, $erakond, $tulemus
+		);
+		$infoTable->addRow($rowData);
+	}	
+} 	
+$mysqli->close();	
+//	
 	
-		while($rows = $query->fetch_assoc()){ 
-			$kandid= $rows['kandidaadiID'] ;
-			$nimi=$rows['nimi'];
-			$erakond=$rows['erakond'];
-			$piirkond=$rows['piirkond']; 
-			$tulemus=$rows['tulemus']; 
-			$rowData = array(
-			$piirkond, $nimi, $erakond, $tulemus
-			);
-			$infoTable->addRow($rowData);
-		}	
-	} 	
-	$mysqli->close();
-	//	
-	
-	$chart = Lava::LineChart('infoChart');
-	$chart->datatable($infoTable);
-	$chart->Lava::render('LineChart','infoChart','chart',true);
-	
+$chart = Lava::LineChart('Info')
+        ->setOptions(array(
+	        'datatable' => $infoTable,
+                'title' => 'Statistika'
+                ));	
+	Lava::renderLineChart('Info', 'chart', true);
 ?>
