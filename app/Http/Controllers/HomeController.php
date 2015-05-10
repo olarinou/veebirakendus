df2188080208475ae82e36d05604026382326338
supisaurus <supisau@ut.ee>
2015-05-10 15:55:26 +0300

    Update HomeController.php
    ...

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -96,6 +96,48 @@ public function postotsing()
 	return view('search');
 }	
 
+public function displaychart()
+	{
+	$mysqli = NEW MySQLi('localhost','root','Admin123','vv_db');
+	
+	$lava = new Lavacharts();
+	$infoTable = $lava->DataTable();
+	$infoTable->addStringColumn('Piirkond')
+			->addStringColumn('Kandidaat') 
+            ->addStringColumn('Erakond') 
+            ->addNumberColumn('Hääled');	
+	
+	//		 	
+	$query = $mysqli->query("SELECT k.kandidaadiID, k.nimi, k.erakond, k.piirkond, t.tulemus FROM kandidaadid as k
+	JOIN tulemused as t ON (k.kandidaadiID = t.kandidaadiID)");
+	
+	if($query->num_rows != 0){		
+	
+		while($rows = $query->fetch_assoc()){ 
+			$kandid= $rows['kandidaadiID'] ;
+			$nimi=$rows['nimi'];
+			$erakond=$rows['erakond'];
+			$piirkond=$rows['piirkond']; 
+			$tulemus=$rows['tulemus']; 
+			$rowData = array(
+			$piirkond, $nimi, $erakond, $tulemus
+			);
+			$infoTable->addRow($rowData);
+		}	
+	} 	
+	$mysqli->close();	
+	//	
+	
+	$chart = $lava->LineChart('Info')
+                    ->setOptions(array(
+                        'datatable' => $infoTable,
+                        'title' => 'Statistika'
+                     ));	
+	
+	return view( 'statistika', array('lava'=>$lava));
+	}
+
+
 public function statistika()
 	{
 		return view('statistika');
