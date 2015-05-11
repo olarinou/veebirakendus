dcf363aa3292058e6fe287642e6185127d0f9fff
Vahvel <tallatatt@gmail.com>
2015-05-11 11:33:09 +0300

    statistika ei streami enam

--- resources/views/statistika.blade.php
+++ resources/views/statistika.blade.php
@@ -2,16 +2,13 @@
 <script src="js/statsstream.js"></script>
 @section('main')
 	<div class="container">
-		<select id="valik">
+		<select id="valik" onchange="statsRetrival(this.value)">			
 			<option value="Riik">Riigis</option>
     		<option value="Piirkond">Piirkondades</option>
     		<option value="Partei">Parteides</option>
    			<option value="Kandidaat">Kandidaatides</option>   						
 		</select>
 	<br>
-	<p id="result"></p>
-	<script>
-	dataRetrival();
-	</script>
+	<p id="result"></p>	
 	</div>
 @stop
