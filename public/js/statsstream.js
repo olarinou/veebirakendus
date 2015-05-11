58e499903bb1ce8b75a884977e0028c92a8a2dae
Vahvel <tallatatt@gmail.com>
2015-05-11 15:07:48 +0300

    test

--- public/js/statsstream.js
+++ public/js/statsstream.js
@@ -33,17 +33,26 @@ function responseAjax() {
   }
   
  function statsRetrival() {
-	var e = document.getElementById("ala");
+    
+        if (window.XMLHttpRequest) {
+            // code for IE7+, Firefox, Chrome, Opera, Safari
+            xmlhttp = new XMLHttpRequest();
+        } else {
+            // code for IE6, IE5
+            xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+        }
+        xmlhttp.onreadystatechange = function() {
+            if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+                document.getElementById("result").innerHTML = xmlhttp.responseText;
+            }
+        }
+		var e = document.getElementById("ala");
 		var ala = e.options[e.selectedIndex].value;
 		e = document.getElementById("partei");
 		var partei = e.options[e.selectedIndex].value;
 		e = document.getElementById("lisa");
 		var lisa = e.options[e.selectedIndex].value;
-	var myRequest = new XMLHttpRequest();
-		myRequest.open("GET", "/data2/"+ala+"/"+partei+"/"+lisa, true);
-		myRequest.onreadystatechange = responseAjax;
-        myRequest.setRequestHeader("Cache-Control", "no-cache");
-		myRequest.send(null);   
-        
+        xmlhttp.open("GET","/data2/Harju/Merkuur/kandidaat",true);
+        xmlhttp.send();
     }
 } 
