edeae7584971d82ff5c7c72d7c23a3e278f4b189
Vahvel <tallatatt@gmail.com>
2015-05-11 12:05:55 +0300

    test

--- resources/views/statistika.blade.php
+++ resources/views/statistika.blade.php
@@ -2,6 +2,8 @@
 <script src="js/statsstream.js"></script>
 @section('main')
 	<div class="container">
+		<table style="width:100%">
+		<tr>
 		<select name="valik" onchange="statsRetrival(this.value)">	
 			<option value="">Häälte jagunemine:</option>
 			<option value="Riik">Riigis</option>
@@ -9,6 +11,18 @@
     		<option value="Partei">Parteides</option>
    			<option value="Kandidaat">Kandidaatides</option>   						
 		</select>
+		</tr>
+		<tr>
+		<select name="valik" onchange="statsRetrival(this.value)">	
+			<option value="">Häälte jagunemine:</option>
+			<option value="Riik">Riigis</option>
+    		<option value="Piirkond">Piirkondades</option>
+    		<option value="Partei">Parteides</option>
+   			<option value="Kandidaat">Kandidaatides</option>   						
+		</select>
+		</tr>
+		</table>		
+		
 	<br>
 	<p id="result"></p>	
 	</div>
