ce186e1e9e8ab839c63074a4abf18ff59c3f41ff
Olari <olari93@gmail.com>
2015-05-13 10:47:56 +0300

    test

--- public/js/addkandidaadid.js
+++ public/js/addkandidaadid.js
@@ -1,6 +1,6 @@
 
 function formValidation() {
-	var controlReg = /^[a-zA-ZÄÜÕÕöäüõó' ]*$/;
+	var controlReg = /^[azAZÄÜÕÕöäüõó' ]*$/;
 	var whiteReg = /^\s+$/;
 	var nimi = document.getElementById("nimi").value;
 	var erakond = document.getElementById("erakond").value;
