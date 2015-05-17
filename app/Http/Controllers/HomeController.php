17241b54a46f8a414617b013643b5d34e6940dcb
Vahvel <tallatatt@gmail.com>
2015-05-17 19:37:48 +0300

    test

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -281,7 +281,7 @@ public function data($opt)
 					echo "data:	{$erakond}<br>\n";
 					echo "data:	{$piirkond}<br>\n";
 					echo "data:	{$tulemus}<br>\n";
-					echo "data:	 <pla>\n";			
+					echo "data:	 </pla>\n";			
 				}	
 			}
 		}
