f6017b2162810506e846f8ccca1b04a62725b484
Vahvel <tallatatt@gmail.com>
2015-05-17 23:36:15 +0300

    tegemata lehed ka

--- resources/views/haaletus.blade.php
+++ resources/views/haaletus.blade.php
@@ -2,9 +2,9 @@
 @section('main')
         <div class="container"> 
 		<div class="col-md-8 col-md-offset-2 form-content">
-		<div class='text-center'>
+		
             @include('h22leta_db')	
-		</div>
+		
 		</div>	
         </div>
 @stop
--- resources/views/kandidaatkinnitatud.blade.php
+++ resources/views/kandidaatkinnitatud.blade.php
@@ -1,6 +1,10 @@
 @extends('master') 
 @section('main')
 		<div class="container">	
+		<div class="col-md-8 col-md-offset-2 form-content">
+		<div class='text-center'>
 			<p>Kandidaat edukalt lisatud!</p>
+			</div>
+		</div>
 		</div>
 @stop
