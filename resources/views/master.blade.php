9d3c50cf07ea92c20c4f246ae614963c44dc0b59
Vahvel <tallatatt@gmail.com>
2015-05-18 00:23:47 +0300

    test

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -9,14 +9,12 @@
 <body>
 	<div class="nav">
 	<div class="container">	
-	<div class="col-md-8 col-md-offset-2 form-content">
 	@if (Auth::check())
 		@include('menuloggedin')
 	@else    
         @include('menuloggedout')
 	@endif	
     </div>
-	</div>
 	</div>
 	<div class="body">
     <div class="container">
@@ -25,10 +23,8 @@
 	</div>
 	<div class="footer">
 		<div class="container">
-		<div class="col-md-8 col-md-offset-2 form-content">
   		<p>Veebivalimised on loodud aine Veebirakenduste loomine korras, 
 		<a href="http://katolilly.tumblr.com/post/109805392949/">kasutatud planeedipiltide allikas.</a></p>
-		</div>
 		</div>
 	</div>
 </body>
