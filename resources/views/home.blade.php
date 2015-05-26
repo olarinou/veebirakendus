75400fbb094cfa510fde765ec4d11116b30f6076
Olari <olari93@gmail.com>
2015-05-26 22:30:51 +0300

    test

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,11 +2,13 @@
 @section('main')
  
 <div class="container">
+    <div class="col-xs-12 col-sm-7 col-md-12 col-lg-12">
     <div class="row">
          <div class="col-md-12">
             <h2>Veebivalimised</h2>
 			<p>2015 aasta valimised on möödas. Õnnitleme kõiki väljavalituid. Järgmine kord uuesti. Teie veebivalimised!</p>	
          </div>
+    </div>
     </div>
 </div>
 
